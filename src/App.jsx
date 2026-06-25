b6de83e0590f9422acd3fe24473e7dd429a15af8
cal <sakphea05@gmail.com>
2026-06-25 17:05:16 +0700

    feat(app): import relevant pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import AddRecipe from "./pages/AddRecipe";
+import Catalog from "./pages/Catalog";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import RecipeDetails from "./pages/RecipeDetails";
+
 function App() {
   return <div>App View</div>;
 }
