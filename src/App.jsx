baadd7a31619d4c0af7551283011ebf24e826d4d
Cal <sakphea05@gmail.com>
2026-06-25 20:16:49 +0700

    feat: implement navbar routing between pages
    - **feat(app): import relevant pages**
    - **feat(app): route pages to their own links**
    - **feat(navbar): add navbar component**

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import AddRecipe from "./pages/AddRecipe";
+import Catalog from "./pages/Catalog";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import RecipeDetails from "./pages/RecipeDetails";
+
 function App() {
-  return <div>App View</div>;
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Catalog />} path="/recipes" />
+        <Route element={<RecipeDetails />} path="/recipes/:id" />
+        <Route element={<AddRecipe />} path="/add" />
+        <Route element={<Favorites />} path="/favorites" />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
