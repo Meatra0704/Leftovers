59684225922db39f0cd8d4b7d9d1549df38ce053
Meatra <meatra0704@gmail.com>
2026-06-29 09:32:13 +0700

    style: format AddRecipe

--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -1,74 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, } from "react";
 
-export default function AddRecipe () {
-    const [formData, setFormData] = useState({
-        title: '',
-        ingredients: '',
-        instructions:'',
-        imageUrl: '',
+export default function AddRecipe() {
+  const [formData, setFormData] = useState({
+    title: "",
+    ingredients: "",
+    instructions: "",
+    imageUrl: "",
+  });
+
+  const { title, ingredients, instructions, imageUrl } = formData;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Successfully submitted:", formData);
+  }
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
+  }
 
-    const { title, ingredients, instructions, imageUrl} = formData;
-
-    function handleSubmit (e) {
-        e.preventDefault();
-        console.log('Successfully submitted:', formData);
-    };
-
-    function handleChange (e) {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Title:
-                    <input
-                    name="title"
-                    placeholder="Pizza..."
-                    value={title}
-                    onChange={handleChange}
-                    >
-                    </input>
-                </label>
-                <label>
-                    Ingredients:
-                    <input
-                    name="ingredients"
-                    placeholder="Lists your ingredients"
-                    value={ingredients}
-                    onChange={handleChange}
-                    ></input>
-                </label>
-                <label>
-                    Instructions:
-                    <input
-                    name="instructions"
-                    placeholder="Step 1..."
-                    value={instructions}
-                    onChange={handleChange}
-                    >
-                    </input>
-                </label>
-                <label>
-                    Share your results:
-                    <input
-                    name="imageUrl"
-                    placeholder="Place your image..."
-                    value={imageUrl}
-                    onChange={handleChange}
-                    >
-                    </input>
-                </label>
-                <button type="submit">Submit Recipe</button>
-            </form>
-        </>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input
+          name="title"
+          onChange={handleChange}
+          placeholder="Pizza..."
+          value={title}
+        />
+      </label>
+      <label>
+        Ingredients:
+        <input
+          name="ingredients"
+          onChange={handleChange}
+          placeholder="Lists your ingredients"
+          value={ingredients}
+        />
+      </label>
+      <label>
+        Instructions:
+        <input
+          name="instructions"
+          onChange={handleChange}
+          placeholder="Step 1..."
+          value={instructions}
+        />
+      </label>
+      <label>
+        Share your results:
+        <input
+          name="imageUrl"
+          onChange={handleChange}
+          placeholder="Place your image..."
+          value={imageUrl}
+        />
+      </label>
+      <button type="submit">Submit Recipe</button>
+    </form>
+  );
 }
-
-
-
