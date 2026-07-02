dc9719c8c48fe00b0d1658fbe1e1ab0a12a17bea
Meatra <meatra0704@gmail.com>
2026-07-02 15:29:01 +0700

    refactor: improve add recipe fields

--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -1,58 +1,116 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { RecipeContext } from "../context/RecipeContext";
 
 export default function AddRecipe() {
-  
   const [ingredients, setIngredients] = useState([
-    { name: "", amount: "", measurement: "cup" },
+    { id: Date.now(), name: "", amount: "", measurement: "cup" },
   ]);
-  const [steps, setSteps] = useState([""]);
+  const [steps, setSteps] = useState([{ id: Date.now(), text: "" }]);
   const [title, setTitle] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [error, setError] = useState("");
-
-
+  const { recipes, addRecipe } = useContext(RecipeContext);
 
   //Handle Ingredient Changes
-  const handleIngredientChange = (index, field, value) => {
-    const updatedIngredients = [...ingredients];
-    updatedIngredients[index][field] = value;
-    setIngredients(updatedIngredients);
+  const handleIngredientChange = (id, field, value) => {
+    // const updatedIngredients = [...ingredients];
+    // updatedIngredients[id][field] = value;
+    // setIngredients(updatedIngredients);
+    setIngredients(
+      ingredients.map((ing) =>
+        ing.id === id ? { ...ing, [field]: value } : ing,
+      ),
+    );
   };
 
   const addIngredientRow = () => {
     setIngredients([
       ...ingredients,
-      { name: "", amount: "", measurement: "cup" },
+      { id: Date.now(), name: "", amount: "", measurement: "cup" },
     ]);
   };
 
-  const removeIngredientRow = (index) => {
-    setIngredients(ingredients.filter((_, i) => i !== index));
+  const removeIngredientRow = (id) => {
+    //If last row reset the input
+    if (ingredients.length <= 1) {
+      setIngredients([
+        { id: Date.now(), name: "", amount: "", measurement: "cup" },
+      ]);
+      return;
+    }
+    setIngredients(ingredients.filter((ing) => ing.id !== id));
   };
 
   //Handle Instructions Changes
-  const handleStepChange = (index, value) => {
-    const updatedInstructions = [...steps];
-    updatedInstructions[index] = value;
-    setSteps(updatedInstructions);
+  const handleStepChange = (id, value) => {
+    setSteps(
+      steps.map((step) => (step.id === id ? { ...step, text: value } : step)),
+    );
   };
 
   const addStepRow = () => {
-    setSteps([...steps, ""]);
+    setSteps([...steps, { id: Date.now(), text: "" }]);
   };
 
-  const removeStepRow = (index) => {
-    setSteps(steps.filter((_, i) => i !== index));
+  const removeStepRow = (id) => {
+    if (steps.length <= 1) {
+      setSteps([{ id: Date.now(), text: "" }]);
+      return;
+    }
+    setSteps(steps.filter((step) => step.id !== id));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Successfully submitted:", { title, ingredients, steps, imageUrl });
+    console.log("Successfully submitted:", {
+      title,
+      ingredients,
+      steps,
+      imageUrl,
+    });
 
     if (!title.trim()) {
       setError("Please enter a title for your recipe.");
       return;
     }
+
+    const validIngredient = ingredients.filter((ing) => ing.name.trim() !== "");
+    const validStep = steps.filter((step) => step.text.trim() === !"");
+
+    if (validIngredient.length === 0 && validStep.length === 0) {
+      setError("Must provide ingredients and instructions before submitting");
+      return;
+    }
+
+    const isDuplicate = recipes.some(
+      (r) => r.title.toLowerCase() === title.toLowerCase().trim(),
+    );
+    if (isDuplicate) {
+      setError("This recipe already exists");
+      return;
+    }
+
+    setError("");
+
+    const newRecipe = {
+      id: Date.now().toString(),
+      title: title.trim(),
+      ingredients: validIngredient,
+      step: validStep,
+    };
+
+    addRecipe(newRecipe);
+
+    setTitle("");
+    setImageUrl("");
+    setIngredients([
+      {
+        name: "",
+        amount: "",
+        measurement: "",
+      },
+    ]);
+    setSteps([""]);
   };
 
   const handleChange = (e) => {
@@ -60,76 +118,85 @@ export default function AddRecipe() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Title:
-        <input
-          name="title"
-          onChange={handleChange}
-          placeholder="Pizza..."
-          value={title}
-        />
-      </label>
+    <>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input
+            name="title"
+            onChange={handleChange}
+            placeholder="Pizza..."
+            value={title}
+          />
+        </label>
 
-      <fieldset>
-        <legend>Ingredients</legend>
-        {ingredients.map((ingredient, index) => (
-          <div key={index}>
-            <input
-              onChange={(e) =>
-                handleIngredientChange(index, "name", e.target.value)
-              }
-              placeholder="Ingredient (e.g. Salmon)"
-              value={ingredient.name}
-            />
-            <input
-              onChange={(e) =>
-                handleIngredientChange(index, "amount", e.target.value)
-              }
-              placeholder="Qty"
-              type="number"
-              value={ingredient.amount}
-            />
-            <button onClick={() => removeIngredientRow(index)} type="button">
-              Remove
-            </button>
-          </div>
-        ))}
-        <button onClick={addIngredientRow} type="button">
-          Add Ingredient
-        </button>
-      </fieldset>
+        <fieldset>
+          <legend>Ingredients</legend>
+          {ingredients.map((ingredient) => (
+            <div key={ingredient.id}>
+              <input
+                onChange={(e) =>
+                  handleIngredientChange(ingredient.id, "name", e.target.value)
+                }
+                placeholder="Ingredient (e.g. Salmon)"
+                value={ingredient.name}
+              />
+              <input
+                onChange={(e) =>
+                  handleIngredientChange(
+                    ingredient.id,
+                    "amount",
+                    e.target.value,
+                  )
+                }
+                placeholder="Qty"
+                type="number"
+                value={ingredient.amount}
+              />
+              <button
+                onClick={() => removeIngredientRow(ingredient.id)}
+                type="button"
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+          <button onClick={addIngredientRow} type="button">
+            Add Ingredient
+          </button>
+        </fieldset>
 
-      <fieldset>
-        <legend>Instructions</legend>
-        {steps.map((step, index) => (
-          <div key={index}>
-            <input
-              onChange={(e) => handleStepChange(index, e.target.value)}
-              placeholder="Add..."
-              value={step}
-            />
-            <button onClick={() => removeStepRow(index)} type="button">
-              Remove
-            </button>
-          </div>
-        ))}
-        <button onClick={addStepRow} type="button">
-          Add Step
-        </button>
-      </fieldset>
+        <fieldset>
+          <legend>Instructions</legend>
+          {steps.map((step) => (
+            <div key={step.id}>
+              <input
+                onChange={(e) => handleStepChange(step.id, e.target.value)}
+                placeholder="Add..."
+              />
+              <button onClick={() => removeStepRow(step.id)} type="button">
+                Remove
+              </button>
+            </div>
+          ))}
+          <button onClick={addStepRow} type="button">
+            Add Step
+          </button>
+        </fieldset>
 
-      <label>
-        Share your results:
-        <input
-          name="imageUrl"
-          onChange={(e) => setImageUrl(e.target.value)}
-          placeholder="Place your image..."
-          value={imageUrl}
-        />
-      </label>
+        <label>
+          Share your results:
+          <input
+            name="imageUrl"
+            onChange={(e) => setImageUrl(e.target.value)}
+            placeholder="Place your image..."
+            value={imageUrl}
+          />
+        </label>
 
-      <button type="submit">Submit Recipe</button>
-    </form>
+        <button type="submit">Submit Recipe</button>
+      </form>
+    </>
   );
 }
