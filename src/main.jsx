ec39963bb0f5a714c3214862d46d0cb6a0f74ccc
Cal <sakphea05@gmail.com>
2026-07-02 13:51:04 +0700

    feat(fonts): add shantell-sans and fredoka fonts

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+import "@fontsource-variable/shantell-sans";
+import "@fontsource-variable/fredoka";
+
 import "./styles/reset.css";
 
 createRoot(document.getElementById("root")).render(
