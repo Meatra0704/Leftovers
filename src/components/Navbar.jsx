154a33c2ca9b42389225c3cb7f9f98530c70f231
cal <sakphea05@gmail.com>
2026-07-02 15:00:17 +0700

    feat(navbar): add navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Navbar.css";
+
 export default function Navbar() {
   return (
     <nav className="nav">
-      <ul className="nav__list">
-        <li className="nav__item">
-          <Link className="nav__link" to="/">
-            Home
-          </Link>
-        </li>
+      <div className="container nav__inner">
+        <Link className="nav__logo" to="/">
+          Home
+        </Link>
 
-        <li className="nav__item">
-          <Link className="nav__link" to="/recipes">
-            Catalog
-          </Link>
-        </li>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <Link className="nav__link" to="/recipes">
+              Catalog
+            </Link>
+          </li>
 
-        <li className="nav__item">
-          <Link className="nav__link" to="/add">
-            Add Recipe
-          </Link>
-        </li>
+          <li className="nav__item">
+            <Link className="nav__link" to="/favorites">
+              Favorites
+            </Link>
+          </li>
 
-        <li className="nav__item">
-          <Link className="nav__link" to="/favorites">
-            Favorites
-          </Link>
-        </li>
-      </ul>
+          <li className="nav__item">
+            <Link className="nav__link" to="/add">
+              Add Recipe
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
