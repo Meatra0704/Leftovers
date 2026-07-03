5077ce702321d089575fa3faeca703d27396dda1
cal <sakphea05@gmail.com>
2026-07-03 13:22:40 +0700

    refactor: add unit select options for ingredients qty

--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -3,12 +3,10 @@ import { RecipeContext } from "../context/RecipeContext";
 
 export default function AddRecipe() {
   const setIngredientsState = () => {
-    return {id: Date.now(), name: "", amount: "", measurement: "whole"}
-  }
+    return { id: Date.now(), name: "", amount: "", unit: "whole" };
+  };
 
-  const [ingredients, setIngredients] = useState([
-    setIngredientsState()
-  ]);
+  const [ingredients, setIngredients] = useState([setIngredientsState()]);
   const [steps, setSteps] = useState([{ id: Date.now(), text: "" }]);
   const [title, setTitle] = useState("");
   const [imageUrl, setImageUrl] = useState("");
@@ -28,18 +26,13 @@ export default function AddRecipe() {
   };
 
   const addIngredientRow = () => {
-    setIngredients([
-      ...ingredients,
-      setIngredientsState()
-    ]);
+    setIngredients([...ingredients, setIngredientsState()]);
   };
 
   const removeIngredientRow = (id) => {
     //If last row reset the input
     if (ingredients.length <= 1) {
-      setIngredients([
-      setIngredientsState()
-      ]);
+      setIngredients([setIngredientsState()]);
       return;
     }
     setIngredients(ingredients.filter((ing) => ing.id !== id));
@@ -102,14 +95,7 @@ export default function AddRecipe() {
 
     setTitle("");
     setImageUrl("");
-    setIngredients([
-      {
-        id: Date.now(),
-        name: "",
-        amount: "",
-        measurement: "whole",
-      },
-    ]);
+    setIngredients([setIngredientsState()]);
     setSteps([""]);
   };
 
@@ -143,6 +129,7 @@ export default function AddRecipe() {
                 value={ingredient.name}
               />
               <input
+                min="0"
                 onChange={(e) =>
                   handleIngredientChange(
                     ingredient.id,
@@ -151,9 +138,44 @@ export default function AddRecipe() {
                   )
                 }
                 placeholder="Qty"
+                step="any"
                 type="number"
                 value={ingredient.amount}
               />
+
+              <select
+                onChange={(e) =>
+                  handleIngredientChange(ingredient.id, "unit", e.target.value)
+                }
+                value={ingredient.unit}
+              >
+                <option value="whole">Whole / Item</option>
+                <option value="clove">Clove(s)</option>
+                <option value="pinch">Pinch / Dash</option>
+                <optgroup label="US Volume">
+                  <option value="tsp">Teaspoon (tsp)</option>
+                  <option value="tbsp">Tablespoon (tbsp)</option>
+                  <option value="fl oz">Fluid Ounce (fl oz)</option>
+                  <option value="cup">Cup</option>
+                  <option value="pint">Pint</option>
+                  <option value="qt">Quart (qt)</option>
+                  <option value="gal">Gallon (gal)</option>
+                </optgroup>
+                <optgroup label="US Weight">
+                  <option value="oz">Ounce (oz)</option>
+                  <option value="lb">Pound (lb)</option>
+                </optgroup>
+                <optgroup label="Metric Volume">
+                  <option value="ml">Milliliter (ml)</option>
+                  <option value="l">Liter (L)</option>
+                </optgroup>
+                <optgroup label="Metric Weight">
+                  <option value="mg">Milligram (mg)</option>
+                  <option value="g">Gram (g)</option>
+                  <option value="kg">Kilogram (kg)</option>
+                </optgroup>
+              </select>
+
               <button
                 onClick={() => removeIngredientRow(ingredient.id)}
                 type="button"
@@ -169,17 +191,20 @@ export default function AddRecipe() {
 
         <fieldset>
           <legend>Instructions</legend>
-          {steps.map((step) => (
+          {steps.map((step, index) => (
             <div key={step.id}>
+              <span>{index + 1}.</span>
               <input
                 onChange={(e) => handleStepChange(step.id, e.target.value)}
                 placeholder="Add..."
+                value={step.text}
               />
               <button onClick={() => removeStepRow(step.id)} type="button">
                 Remove
               </button>
             </div>
           ))}
+
           <button onClick={addStepRow} type="button">
             Add Step
           </button>
