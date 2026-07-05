64df04fa69e82fd0f0e30d9869a2ed7c2e762185
Meatra <meatra0704@gmail.com>
2026-07-05 13:09:48 +0700

    refactor: align recipe card props and add ingredients display

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,10 +1,14 @@
-function RecipeCard({ titles, ingredient, imageUrl }) { 
+export default function RecipeCard({ title, ingredients, imageUrl }) {
   return (
-    <>
-      <div className="card">
-        <img alt="food" src={imageUrl}></img>
-        <h3>{titles}</h3>
-      </div>
-    </>
+    <div className="card">
+      <img alt="food" src={imageUrl}></img>
+      <h3>{title}</h3>
+      <p>{ingredients.name}</p>
+      <ul>
+        {ingredients.map((ing) => (
+          <li key={ing.id}>{ing.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,17 +1,20 @@
 import React, { useContext } from "react";
 import RecipeCard from "../components/RecipeCard";
 import { RecipeContext, RecipeProvider } from "../context/RecipeContext";
-
+  
 export default function Catalog() {
   const { recipes } = useContext(RecipeContext);
 
   return (
-    <>
-      <div className="catalog-container">
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
-      </div>
-    </>
+    <div className="catalog-container">
+      {recipes.map((recipe) => (
+        <RecipeCard
+          imageUrl={recipe.imageUrl}
+          ingredients={recipe.ingredients}
+          key={recipe.id}
+          title={recipe.title}
+        />
+      ))}
+    </div>
   );
 }
