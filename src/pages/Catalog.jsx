d76f2dafe635c55e8fcb41c32860fbbb06a9fe32
Meatra <meatra0704@gmail.com>
2026-07-05 17:32:13 +0700

    feat: pass id attribute in recipe card

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,20 +1,28 @@
 import React, { useContext } from "react";
+import PageHeader from "../components/PageHeader";
 import RecipeCard from "../components/RecipeCard";
 import { RecipeContext, RecipeProvider } from "../context/RecipeContext";
-  
+
 export default function Catalog() {
   const { recipes } = useContext(RecipeContext);
 
   return (
-    <div className="catalog-container">
-      {recipes.map((recipe) => (
-        <RecipeCard
-          imageUrl={recipe.imageUrl}
-          ingredients={recipe.ingredients}
-          key={recipe.id}
-          title={recipe.title}
-        />
-      ))}
+    <div className="container page-view">
+      <div className="catalog">
+        <PageHeader
+          subtitle="Explore your favorite cusine"
+          title="Catalog"
+        ></PageHeader>
+        {recipes.map((recipe) => (
+          <RecipeCard
+            id={recipe.id}
+            imageUrl={recipe.imageUrl}
+            ingredients={recipe.ingredients}
+            key={recipe.id}
+            title={recipe.title}
+          />
+        ))}
+      </div>
     </div>
   );
 }
