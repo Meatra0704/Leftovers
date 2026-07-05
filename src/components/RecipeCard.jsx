7e4431cf89bfaaf1fce91cddabf64501e72ac26b
Meatra <meatra0704@gmail.com>
2026-07-05 16:47:18 +0700

    feat: add classes to recipe card

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,13 +1,17 @@
 export default function RecipeCard({ title, ingredients, imageUrl }) {
   return (
-    <div className="card">
-      <img alt={title} src={imageUrl}></img>
-      <h3>{title}</h3>
-      <ul>
-        {ingredients.map((ing) => (
-          <li key={ing.id}>{ing.name}</li>
-        ))}
-      </ul>
+    <div className="recipe-card">
+      <img alt={title} className="recipe-card__image" src={imageUrl}></img>
+      <div className="recipe-card__content">
+        <h3 className="recipe-card__title">{title}</h3>
+        <ul className="recipe-card__list">
+          {ingredients.map((ing) => (
+            <li className="recipe-card__item" key={ing.id}>
+              {ing.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
