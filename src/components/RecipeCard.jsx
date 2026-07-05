a84f98dfe529e258b2a7885290a629ea68fcd3c5
Meatra <meatra0704@gmail.com>
2026-07-05 16:34:19 +0700

    refactor: fix display ingredient

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -3,7 +3,6 @@ export default function RecipeCard({ title, ingredients, imageUrl }) {
     <div className="card">
       <img alt="food" src={imageUrl}></img>
       <h3>{title}</h3>
-      <p>{ingredients.name}</p>
       <ul>
         {ingredients.map((ing) => (
           <li key={ing.id}>{ing.name}</li>
