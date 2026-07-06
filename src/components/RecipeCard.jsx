9fcf8e77afc00153a8854135959fe20b7819fe71
Meatra <meatra0704@gmail.com>
2026-07-06 11:16:15 +0700

    style: design recipe card layout

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import "./RecipeCard.css";
+
 export default function RecipeCard({ title, ingredients, imageUrl }) {
   return (
     <article className="recipe-card">
       <img alt={title} className="recipe-card__image" src={imageUrl}></img>
       <div className="recipe-card__content">
         <h3 className="recipe-card__title">{title}</h3>
+        <h4 className="recipe-card__ingredient-head">Main Ingredients</h4>
         <ul className="recipe-card__list">
           {ingredients.map((ing) => (
             <li className="recipe-card__item" key={ing.id}>
