d86d0bcc6a118bcb631ca494e1c1271c2e830d59
Meatra0704 <meatra0704@gmail.com>
2026-07-06 11:54:26 +0700

    feat/catalog view (#16)
    - feat: implement catalog view component
    - refactor: align recipe card props and add ingredients display
    - refactor: fix display ingredient
    - fix: use title alt attribute in image tag
    - feat: add classes to recipe card
    - feat: add page header component
    - feat: pass id attribute in recipe card
    
    * refactor: change div tag to article in recipe card
    
    * feat: add recipe grid class to hold recipes
    
    * style: design recipe card layout

---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./PageHeader.css";
+
+export default function PageHeader({ title, subtitle }) {
+  return (
+    <header className="page-header">
+      <h1 className="page-header__title">{title}</h1>
+      <p className="page-header__subtitle">{subtitle}</p>
+    </header>
+  );
+}
---
+++ src/pages/Catalog.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import PageHeader from "../components/PageHeader";
+import RecipeCard from "../components/RecipeCard";
+import { RecipeContext, RecipeProvider } from "../context/RecipeContext";
+
+export default function Catalog() {
+  const { recipes } = useContext(RecipeContext);
+
+  return (
+    <div className="container page-view">
+      <div className="catalog">
+        <PageHeader
+          subtitle="Explore your favorite cusine"
+          title="Catalog"
+        ></PageHeader>
+        <div className="recipe-grid">
+          {recipes.map((recipe) => (
+            <RecipeCard
+              id={recipe.id}
+              imageUrl={recipe.imageUrl}
+              ingredients={recipe.ingredients}
+              key={recipe.id}
+              title={recipe.title}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
