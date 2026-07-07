b1290cc182260f720d9bd70c83b660a7ef50e7c7
Meatra <meatra0704@gmail.com>
2026-07-07 11:32:17 +0700

    feat: chain filter to map to display items with search bar

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useState } from "react";
 import PageHeader from "../components/PageHeader";
 import RecipeCard from "../components/RecipeCard";
 import { RecipeContext, RecipeProvider } from "../context/RecipeContext";
 
 export default function Catalog() {
   const { recipes } = useContext(RecipeContext);
+  const [searchText, setSearchText] = useState("");
 
   return (
     <main className="container page-view">
@@ -14,18 +14,29 @@ export default function Catalog() {
           subtitle="Explore your favorite cusine"
           title="Catalog"
         ></PageHeader>
+        <input
+          onChange={(e) => setSearchText(e.target.value)}
+          type="text"
+          value={searchText}
+        ></input>
 
         <div className="recipe-grid">
-          {recipes.map((recipe) => (
-            <RecipeCard
-              id={recipe.id}
-              imageUrl={recipe.imageUrl}
-              ingredients={recipe.ingredients}
-              isFavorite={recipe.isFavorite}
-              key={recipe.id}
-              title={recipe.title}
-            />
-          ))}
+          {recipes
+            .filter((recipe) => {
+              const matchSearch = recipe.title
+                .toLowerCase()
+                .includes(searchText.toLowerCase());
+              return matchSearch;
+            })
+            .map((recipe) => (
+              <RecipeCard
+                id={recipe.id}
+                imageUrl={recipe.imageUrl}
+                ingredients={recipe.ingredients}
+                key={recipe.id}
+                title={recipe.title}
+              />
+            ))}
         </div>
       </section>
     </main>
