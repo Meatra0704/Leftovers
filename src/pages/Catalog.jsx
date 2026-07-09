675e503418df2f8cb530365fd19af055c3e12adf
Meatra0704 <meatra0704@gmail.com>
2026-07-09 15:25:38 +0700

    feat/search catalog (#31)
    - feat: chain filter to map to display items with search bar
    - feat: wrap title and search bar in div and give class name
    - feat: add class name to title and subtitle
    - feat: add css file and style header of catalog
    - feat: add class name to page header
    - refactor: format page header class name
    - feat: style catalog header
    - feat: use find method to check id for recipe item and display name

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,31 +1,49 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useState } from "react";
 import PageHeader from "../components/PageHeader";
 import RecipeCard from "../components/RecipeCard";
 import { RecipeContext, RecipeProvider } from "../context/RecipeContext";
+import "./Catalog.css";
 
 export default function Catalog() {
   const { recipes } = useContext(RecipeContext);
+  const [searchText, setSearchText] = useState("");
 
   return (
     <main className="container page-view">
       <section className="catalog">
-        <PageHeader
-          subtitle="Explore your favorite cusine"
-          title="Catalog"
-        ></PageHeader>
-
+        <div className="catalog__wrapper">
+          <PageHeader
+            className="catalog__header"
+            subtitle="Explore your favorite cusine"
+            subtitleClassName="catalog__subtitle"
+            title="Catalog"
+            titleClassName="catalog__title"
+          ></PageHeader>
+          <input
+            className="catalog__searchBar"
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder="Search"
+            type="text"
+            value={searchText}
+          ></input>
+        </div>
         <div className="recipe-grid">
-          {recipes.map((recipe) => (
-            <RecipeCard
-              id={recipe.id}
-              imageUrl={recipe.imageUrl}
-              ingredients={recipe.ingredients}
-              isFavorite={recipe.isFavorite}
-              key={recipe.id}
-              title={recipe.title}
-            />
-          ))}
+          {recipes
+            .filter((recipe) => {
+              const matchSearch = recipe.title
+                .toLowerCase()
+                .includes(searchText.toLowerCase());
+              return matchSearch;
+            })
+            .map((recipe) => (
+              <RecipeCard
+                id={recipe.id}
+                imageUrl={recipe.imageUrl}
+                ingredients={recipe.ingredients}
+                key={recipe.id}
+                title={recipe.title}
+              />
+            ))}
         </div>
       </section>
     </main>
---
+++ src/pages/RecipeDetails.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { RecipeContext, RecipeProvider } from "../context/RecipeContext";
+
+export default function RecipeDetials() {
+  const { id } = useParams();
+  const { recipes } = useContext(RecipeContext);
+
+  const currentRecipe = recipes.find((recipe) => recipe.id === id);
+
+  return (
+    <main>
+      <img alt={currentRecipe.title} src={currentRecipe.imageUrl}></img>
+      <h1>{currentRecipe.title}</h1>
+      <h2>Ingredients</h2>
+      <ul>
+        {currentRecipe.ingredients.map((ing) => (
+          <li key={ing.id}>
+            {ing.amount} {ing.unit} of {ing.name}
+          </li>
+        ))}
+      </ul>
+      <h2>Steps</h2>
+      <ol>
+        {currentRecipe.steps.map((step) => (
+          <li key={step.id}>{step}</li>
+        ))}
+      </ol>
+    </main>
+  );
+}
