d331f52114847dd3b1ea9c961b6006504a0d3e5a
Meatra <meatra0704@gmail.com>
2026-07-10 17:07:13 +0700

    feat: add icon for ingredient and direction

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { RecipeContext, RecipeProvider } from "../context/RecipeContext";
+import "./RecipeDetails.css";
+import { ListChecks, ShoppingBasket } from "lucide-react";
 
 export default function RecipeDetials() {
   const { id } = useParams();
@@ -12,15 +14,20 @@ export default function RecipeDetials() {
     <main className="container page-view">
       <section className="recipe-detail">
         <div className="recipe-detail__image-box">
-          <h1 className="recipe-detail__title">{currentRecipe.title}</h1>
           <img
             alt={currentRecipe.title}
             className="recipe-detail__image"
             src={currentRecipe.imageUrl}
           ></img>
+          <h1 className="recipe-detail__title">{currentRecipe.title}</h1>
         </div>
         <div className="recipe-detail__ingredient-box">
-          <h2 className="recipe-detail__ingredient-title">Ingredients</h2>
+          <header className="recipe-detail__ingredient-header">
+            <span className="icon-basket">
+              <ShoppingBasket size={35}/>
+            </span>
+            <h2 className="recipe-detail__ingredient-title">Ingredients</h2>
+          </header>
           <ul className="recipe-detail__ingredient-list">
             {currentRecipe.ingredients.map((ing) => (
               <li key={ing.id}>
@@ -30,10 +37,20 @@ export default function RecipeDetials() {
           </ul>
         </div>
         <div className="recipe-detail__step-box">
-          <h2 className="recipe-detail__step-title">Steps</h2>
-          <ol className="recipe-detial__step-list">
-            {currentRecipe.steps.map((step) => (
-              <li key={step.id}>{step}</li>
+          <header className="recipe-detail__step-header">
+            <span className="icon-list">
+              <ListChecks size={35} />
+            </span>
+            <h2 className="recipe-detail__step-title">Directions</h2>
+          </header>
+          <ol className="recipe-detail__step-list">
+            {currentRecipe.steps.map((step, index) => (
+              <div className="recipe-detail__step-item" key={index}>
+                <h2 className="recipe-detail__step-item-title">
+                  Step {index + 1}
+                </h2>
+                <p key={step.id}>{step}</p>
+              </div>
             ))}
           </ol>
         </div>
